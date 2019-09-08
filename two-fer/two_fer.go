0f1e13ebc0f2ec74ff5d236bc2aaa5b92573cb06
Shrivatsa <shrivatsa.swadi@gmail.com>
2019-09-08 15:47:44 +0530

    Fixed comments in two_fer

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,8 +1,11 @@
+// Package twofer returns a two for one.
+// One for `name` and one for me.
 package twofer
 
 import "fmt"
 
-// ShareWith should have a comment documenting it.
+// ShareWith takes a name and returns a two-fer string.
+// The input defaults to 'you', if input is empty.
 func ShareWith(name string) string {
 	var you = "you"
 	if name != "" {
